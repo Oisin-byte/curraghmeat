b284fcecfc56545b7d1930da4f2197bdca6728fc
Oisin-byte <oisin@digitalbulb.ie>
2022-08-15 11:36:10 +0100

    new file:   .env.local.example

---
+++ products.js
@@ -0,0 +1,92 @@
+const products = [
+  {
+    id: 'price_1JAE9cEy9j6mAC9ettoD2R0D',
+    name: 'Beef',
+    price: 3995,
+    currency: 'EUR',
+    image: '/meats/beef.jpg',
+    rating: {
+      count: 85,
+      rate: 3.5,
+    },
+  },
+  {
+    id: 'price_1JAEABEy9j6mAC9eYTnD6Rv7',
+    name: 'Lamb',
+    price: 5995,
+    currency: 'EUR',
+    image: '/meats/lamb.jpg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1JAE9cEy9j6mAC9ettoD2R0D',
+    name: 'Beef',
+    price: 3995,
+    currency: 'EUR',
+    image: '/meats/beef.jpg',
+    rating: {
+      count: 85,
+      rate: 3.5,
+    },
+  },
+  {
+    id: 'price_1JAEABEy9j6mAC9eYTnD6Rv7',
+    name: 'Lamb',
+    price: 5995,
+    currency: 'EUR',
+    image: '/meats/lamb.jpg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1JAE9cEy9j6mAC9ettoD2R0D',
+    name: 'Beef',
+    price: 3995,
+    currency: 'EUR',
+    image: '/meats/beef.jpg',
+    rating: {
+      count: 85,
+      rate: 3.5,
+    },
+  },
+  {
+    id: 'price_1JAEABEy9j6mAC9eYTnD6Rv7',
+    name: 'Lamb',
+    price: 5995,
+    currency: 'EUR',
+    image: '/meats/lamb.jpg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1JAE9cEy9j6mAC9ettoD2R0D',
+    name: 'Beef',
+    price: 3995,
+    currency: 'EUR',
+    image: '/meats/beef.jpg',
+    rating: {
+      count: 85,
+      rate: 3.5,
+    },
+  },
+  {
+    id: 'price_1JAEABEy9j6mAC9eYTnD6Rv7',
+    name: 'Lamb',
+    price: 5995,
+    currency: 'EUR',
+    image: '/meats/lamb.jpg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+];
+
+export default products;
